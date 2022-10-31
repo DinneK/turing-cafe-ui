3ff6c45274a5ee8895e637cad0bf74a65047434b
Dinne Kopelevich <dinne22@hotmail.com>
2022-10-31 12:10:53 -0600

    Cypress Testing

--- cypress/e2e/spec_for_mid.cy.js
+++ cypress/e2e/spec_for_mid.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Mid Mod Cafe", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/reservations", {
+      statusCode: 200,
+      ok: true,
+      fixture: "reservations",
+    });
+    cy.visit("http://localhost:3000");
+  });
+
+  it("should display the home page and reservations", () => {
+    cy.get("h1").contains("Turing Cafe Reservations").get("form");
+    cy.get(".singleResy").get("h3").contains("Christie");
+  });
+});
